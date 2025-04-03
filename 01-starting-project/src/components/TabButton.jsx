ce2fea9011c94d2dbb0602c84ac3696f1f586471
byPG <67753466+byPG@users.noreply.github.com>
2025-04-03 21:35:14 +0200

    creating props and basic structure

---
+++ 01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,11 @@
+export default function TabButton({children}){
+    function handleClick() {
+        console.log('hello world');
+    }
+
+    return (
+        <li>
+            <button onClick={handleClick}>{children}</button>
+        </li>
+    )
+}
