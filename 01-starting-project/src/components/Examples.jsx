d334db510580736aee4ebce566192ae6c159fc8a
byPG <67753466+byPG@users.noreply.github.com>
2025-04-17 23:17:44 +0200

    managing with the elements

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./TabButton.jsx";
 import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
+
 
 export default function Examples(){
     const [selectedTopic, setSelectedTopic] = useState('');
@@ -27,13 +29,14 @@ if (selectedTopic) {
 
     return(
         <Section title="Examples" id="examples">
+          <Tabs>{tabContent}</Tabs>
           <menu>
             <TabButton isSelected={selectedTopic === "components"} onClick={() => handleSelect('components')}>Components</TabButton>
             <TabButton isSelected={selectedTopic === "jsx"} onClick={() => handleSelect('jsx')}>JSX</TabButton>
             <TabButton isSelected={selectedTopic === "props"} onClick={() => handleSelect('props')}>Props</TabButton>
             <TabButton isSelected={selectedTopic === "state"} onClick={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {tabContent}
+ 
         </Section>
     );
 }
